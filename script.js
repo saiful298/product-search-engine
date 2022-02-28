19e64a2292dc976b49264515c6044617e62a3d62
saiful298 <prog.saiful@gmail.com>
2022-03-01 04:10:17 +0600

    js working

--- script.js
+++ script.js
@@ -33,5 +33,31 @@ const loadMobileDetail = slugId => {
     const url = `https://openapi.programming-hero.com/api/phone/${slugId}`;
     fetch(url)
         .then(res => res.json())
-        .then(dataList => displaMobileDetail(dataList.data));
+        .then(dataList => allMobileDetail(dataList.data));
+}
+
+const allMobileDetail = slug => {
+    console.log(slug);
+    const mobileDetails = document.getElementById('mobile-details');
+    mobileDetails.textContent = '';
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <img src="${slug.image}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h3 class="card-title">${slug.name}</h3>
+        <h5>${slug.releaseDate}</h5>
+        <h6>WLAN= ${slug.others.WLAN}</h6>
+        <h6>Radio= ${slug.others.Radio}</h6>
+        <h6>USB= ${slug.others.USB}</h6>
+        <h6>GPS= ${slug.others.GPS}</h6>
+        <h6>NFC= ${slug.others.NFC}</h6>
+        <h6>Memory= ${slug.mainFeatures.memory}</h6>
+        <h6>ChipSet= ${slug.mainFeatures.chipSet}</h6>
+        <h6>Storage= ${slug.storage}</h6>
+        <h6>Bluetooth= ${slug.others.Bluetooth}</h6>
+        <h6>Display Size= ${slug.mainFeatures.displaySize}</h6>
+      </div>
+    `;
+    mobileDetails.appendChild(div);
 }
