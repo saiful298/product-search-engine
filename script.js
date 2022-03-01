211598e0a8d804386f2e3eb773b6eb0d4f4a9ba2
saiful298 <prog.saiful@gmail.com>
2022-03-01 11:25:53 +0600

    js working

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
+var error = document.getElementById('error')
 const searchPhone = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     searchField.value = '';
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText} `
-    fetch(url)
-        .then(res => res.json())
-        .then(dataList => displaySearchResult(dataList.data));
+    if (searchText == '') {
+        error.innerText = 'plase enter a value'
+    }
+    else {
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText} `
+        fetch(url)
+            .then(res => res.json())
+            .then(dataList => displaySearchResult(dataList.data));
+    }
 }
 const displaySearchResult = data => {
     const searchResult = document.getElementById('search-result');
     searchResult.innerHTML = '';
+    if (data.legend == 0) {
+
+    }
     data.forEach(slug => {
         // console.log(slug);
         const div = document.createElement('div');
@@ -35,6 +44,7 @@ const loadMobileDetail = slugId => {
         .then(dataList => allMobileDetail(dataList.data));
 }
 const allMobileDetail = slug => {
+    // slug.slice(0, 20)
     console.log(slug);
     const mobileDetails = document.getElementById('mobile-details');
     mobileDetails.textContent = '';
@@ -44,14 +54,19 @@ const allMobileDetail = slug => {
     <img src="${slug.image}" class="card-img-top" alt="...">
       <div class="card-body">
         <h3 class="card-title">${slug.name}</h3>
-        <h5>${slug.releaseDate}</h5>
+        <p>${slug.releaseDate}</p>
         <h6>WLAN= ${slug.others.WLAN}</h6>
         <h6>Radio= ${slug.others.Radio}</h6>
         <h6>USB= ${slug.others.USB}</h6>
         <h6>GPS= ${slug.others.GPS}</h6>
         <h6>NFC= ${slug.others.NFC}</h6>
-        <h6>Memory= ${slug.mainFeatures.memory}</h6>
-        <h6>ChipSet= ${slug.mainFeatures.chipSet}</h6>
+        <h6>Memory= ${slug.mainFeatures.sensors[0]}</h6>
+        <h6>ChipSet= ${slug.mainFeatures.sensors[1]}</h6>
+        <h6>Feature= ${slug.mainFeatures.sensors[2]}</h6>
+        <h6>Feature= ${slug.mainFeatures.sensors[3]}</h6>
+        <h6>Feature= ${slug.mainFeatures.sensors[4]}</h6>
+        <h6>Feature= ${slug.mainFeatures.sensors[5]}</h6>
+        <h6>Feature= ${slug.mainFeatures.sensors[6]}</h6>
         <h6>Storage= ${slug.storage}</h6>
         <h6>Bluetooth= ${slug.others.Bluetooth}</h6>
         <h6>Display Size= ${slug.mainFeatures.displaySize}</h6>
