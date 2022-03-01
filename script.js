2ab5f23a3dd6883f8a1f52dcf72cc4e16542294b
saiful298 <prog.saiful@gmail.com>
2022-03-01 23:50:53 +0600

    js working

--- script.js
+++ script.js
@@ -3,10 +3,8 @@ const searchPhone = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     searchField.value = '';
-    if (searchText == '') {
-        error.innerText = 'plase enter a value'
-    }
-    else {
+    if (searchText) {
+
         const url = `https://openapi.programming-hero.com/api/phones?search=${searchText} `
         fetch(url)
             .then(res => res.json())
@@ -16,25 +14,32 @@ const searchPhone = () => {
 const displaySearchResult = data => {
     const searchResult = document.getElementById('search-result');
     searchResult.innerHTML = '';
-    if (data.legend == 0) {
-
+    if (data.length == 0) {
+        error.innerText = `
+      আপনি যাহা খুজদেআছেন হেইয়া মোগো ধারে নাই
+      `
     }
-    data.forEach(slug => {
-        // console.log(slug);
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
-            <div  class="card h-100">
-                <img src="${slug.image}" class="card-img-top" alt="...">
-                <div class="card-body">
-                <h3 class="card-title">${slug.phone_name}</h3>
-                <h6>Brand: ${slug.brand}</h6>
-                <button onclick="loadMobileDetail('${slug.slug}')" >Details</button>
-                 </div>
-            </div>
-        `;
-        searchResult.appendChild(div);
-    })
+    else {
+        error.innerText = ``
+        data.forEach(slug => {
+            // console.log(slug);
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+                <div  class="card h-100">
+                    <img src="${slug.image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h3 class="card-title">${slug.phone_name}</h3>
+                    <h6>Brand: ${slug.brand}</h6>
+                    <button onclick="loadMobileDetail('${slug.slug}')" >Details</button>
+                     </div>
+                </div>
+            `;
+            searchResult.appendChild(div);
+
+        })
+    }
+
 }
 const loadMobileDetail = slugId => {
     // console.log(slugId)
@@ -60,13 +65,14 @@ const allMobileDetail = slug => {
         <h6>USB= ${slug.others.USB}</h6>
         <h6>GPS= ${slug.others.GPS}</h6>
         <h6>NFC= ${slug.others.NFC}</h6>
-        <h6>Memory= ${slug.mainFeatures.sensors[0]}</h6>
-        <h6>ChipSet= ${slug.mainFeatures.sensors[1]}</h6>
-        <h6>Feature= ${slug.mainFeatures.sensors[2]}</h6>
-        <h6>Feature= ${slug.mainFeatures.sensors[3]}</h6>
-        <h6>Feature= ${slug.mainFeatures.sensors[4]}</h6>
-        <h6>Feature= ${slug.mainFeatures.sensors[5]}</h6>
-        <h6>Feature= ${slug.mainFeatures.sensors[6]}</h6>
+        <h6>Memory= ${slug.mainFeatures.storage}</h6>
+        <h6>sensors= ${slug.mainFeatures.sensors[0]},
+                     ${slug.mainFeatures.sensors[1]},
+                     ${slug.mainFeatures.sensors[2]},
+                     ${slug.mainFeatures.sensors[3]},
+                     ${slug.mainFeatures.sensors[4]}
+                     ${slug.mainFeatures.sensors[5]}
+                     ${slug.mainFeatures.sensors[6]}</h6>
         <h6>Storage= ${slug.storage}</h6>
         <h6>Bluetooth= ${slug.others.Bluetooth}</h6>
         <h6>Display Size= ${slug.mainFeatures.displaySize}</h6>
